79c7a4d3d5e85dd9ddddad9a595dc046d485df7d
Michael Mühlebach <michael@anduin.ch>
2011-09-29 23:25:21 +0200

    revamped the versions view with initial creation of editors.

---
+++ CowGraph/CowGraphWindows/src/zbeans/cowgraph/windows/version/DocumentNode.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2011 Michael Muehlebach <michael@anduin.ch>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+ * $Id$
+ * 
+ * Copyright (c) 2010 Anduin Developments. http://www.anduin.ch
+ */
+package zbeans.cowgraph.windows.version;
+
+import javax.swing.Action;
+import org.openide.nodes.AbstractNode;
+import org.openide.nodes.Children;
+import org.openide.util.lookup.Lookups;
+import zbeans.cowgraph.model.CowGraphDocument;
+
+/**
+ *
+ * @author Michael Muehlebach <michael@anduin.ch>
+ */
+/** Getting the feed node and wrapping it in a FilterNode */
+public class DocumentNode extends AbstractNode {
+
+    DocumentNode(CowGraphDocument document) {
+        super(Children.create(new DocumentChildren(document), true), Lookups.singleton(document));
+    }
+
+    @Override
+    public String getDisplayName() {
+        CowGraphDocument document = getLookup().lookup(CowGraphDocument.class);
+        return document.getName();
+    }
+
+//    @Override
+//    public Image getIcon(int type) {
+//        return ImageUtilities.loadImage("org/netbeans/feedreader/rss16.gif");
+//    }
+//
+//    @Override
+//    public Image getOpenedIcon(int type) {
+//        return getIcon(type);
+//    }
+
+    @Override
+    public Action[] getActions(boolean context) {
+        return new Action[]{/*SystemAction.get(DeleteAction.class)*/};
+    }
+}
